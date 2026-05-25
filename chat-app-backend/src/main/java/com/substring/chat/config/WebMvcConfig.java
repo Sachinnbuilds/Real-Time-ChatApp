7446997fc025db4d4765f955e157826e835ddaab
Sach21605in20 <sachinrc2006@gmail.com>
2026-05-25 16:39:42 +0530

    Separate WebMvc and WebSocket configurations to avoid erasure clash

--- chat-app-backend/src/main/java/com/substring/chat/config/WebMvcConfig.java
+++ chat-app-backend/src/main/java/com/substring/chat/config/WebMvcConfig.java
@@ -1,22 +1,16 @@
 package com.substring.chat.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class RestCorsConfig implements WebMvcConfigurer {
-
-    @Value("${app.frontend.url}")
-    private String frontendUrl;
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(frontendUrl)
+        registry.addMapping("/**") // Applies CORS protection globally to your REST controllers
+                .allowedOrigins("https://real-time-chat-app-git-main-sachin-rameshs-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
